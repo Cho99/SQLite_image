545c955cc7f3e2e60707c263f04d5f04d9ead807
quang anh <gitcho999@gmail.com>
2019-09-02 15:00:12 +0700

    Update_UI

--- app/src/main/java/com/example/sqliteluuhinhanh/ThemDoVatActivity.java
+++ app/src/main/java/com/example/sqliteluuhinhanh/ThemDoVatActivity.java
@@ -3,12 +3,33 @@ package com.example.sqliteluuhinhanh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ThemDoVatActivity extends AppCompatActivity {
+
+    Button btnThem, btnHuy;
+    ImageView imgHinh;
+    ImageButton ibtnCamera, ibtnFolder;
+    EditText edtTen, edtMoTa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_do_vat);
+
+        AnhXa();
+    }
+
+    private void AnhXa() {
+        btnThem = (Button) findViewById(R.id.buttonThem);
+        btnHuy = (Button) findViewById(R.id.buttonHuy);
+        imgHinh = (ImageView) findViewById(R.id.imageView);
+        ibtnCamera  = (ImageButton) findViewById(R.id.imageButtonCamera);
+        ibtnFolder = (ImageButton) findViewById(R.id.imageButtonFolder);
+        edtTen = (EditText) findViewById(R.id.editTextName);
+        edtMoTa = (EditText) findViewById(R.id.editTextDescription);
     }
 }
